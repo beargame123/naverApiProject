0385211fb61d22aaae6d9adde53524d8b76d66c5
beargame123 <hoyoung7827@gmail.com>
2022-06-16 23:46:04 +0900

    🛠️jpa로 전환

--- restaurants/src/main/java/com/example/restaurants/db/MemoryDbEntity.java
+++ restaurants/src/main/java/com/example/restaurants/db/MemoryDbEntity.java
@@ -1,12 +1,21 @@
 package com.example.restaurants.db;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Builder
 public class MemoryDbEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer index;
 }
