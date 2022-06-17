850a4c665362c47738d6a163d23f08731fd03ad6
beargame123 <hoyoung7827@gmail.com>
2022-06-17 23:11:08 +0900

    🛠️ dto 수정

--- restaurants/src/main/java/com/example/restaurants/wishlist/dto/WishListDto.java
+++ restaurants/src/main/java/com/example/restaurants/wishlist/dto/WishListDto.java
@@ -11,8 +11,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class WishListDto {
 
-    private Integer index;
-    private String title;  //음식점
+    private Integer id;
+    private String title;  // 장소이름
     private String category; // 카테고리
     private String address; // 주소
     private String roadAddress; // 도로명
