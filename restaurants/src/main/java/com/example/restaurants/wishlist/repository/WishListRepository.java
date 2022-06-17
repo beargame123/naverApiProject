18bd87f37b31d46760831a92b240749add09f9ae
beargame123 <hoyoung7827@gmail.com>
2022-06-17 23:11:57 +0900

    🛠️ repository 수정

--- restaurants/src/main/java/com/example/restaurants/wishlist/repository/WishListRepository.java
+++ restaurants/src/main/java/com/example/restaurants/wishlist/repository/WishListRepository.java
@@ -4,6 +4,8 @@ import com.example.restaurants.wishlist.entity.WishListEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WishListRepository extends JpaRepository<WishListEntity, Integer> {
 }
